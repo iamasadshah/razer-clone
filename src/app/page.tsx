fae264a58c821ad190f77eec6a9388267fe1d436
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-09-23 02:44:28 +0500

    favicon changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ import Navbar from "./components/Navbar";
 import Secondsection from "./components/Secondsection";
 import Shopnow from "./components/Shopnow";
 import ThirdSection from "./components/ThirdSection";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <div className="space-y-4">
         <Shopnow />
