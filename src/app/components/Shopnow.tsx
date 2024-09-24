15416071e25e08385f3a138fe4edfae63083511e
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-09-24 23:26:40 +0500

    home page completed

--- src/app/components/Shopnow.tsx
+++ src/app/components/Shopnow.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Shopnow = () => {
   return (
-    <div className="flex justify-center items-center text-white py-2 text-base px-8 h-7">
+    <div className="flex items-center justify-center text-white py-2 text-center text-xs lg:text-base px-8 h-7 mt-2 lg:m-0">
       <p>
         For a limited time, all orders will enjoy free shipping to the United
         States.{" "}
